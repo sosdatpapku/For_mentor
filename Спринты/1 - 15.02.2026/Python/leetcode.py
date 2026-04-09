eef1de840137b594852c83f16788fa907d325316
sosdatpapku <metall_hair@mail.ru>
2026-04-09 22:00:00 +0400

    ДЗ 8 недели

--- Спринты/1 - 15.02.2026/Python/leetcode.py
+++ Спринты/1 - 15.02.2026/Python/leetcode.py
@@ -84,24 +84,17 @@ class Solution:
 # 151. Reverse Words in a String
     
 class Solution:
-    def reverseVowels(self, s: str) -> str:
+    def reverseWords(self, s: str) -> str:
+        s = s.split()
+        
         left = 0
         right = len(s) - 1
-        s = list(s)
-        vowels = set('aeiouAEIOU')
 
         while left < right:
-            while left < right and s[left] not in vowels:
-                left += 1
-            
-            while left < right and s[right] not in vowels:
-                right -= 1
-            
             s[left], s[right] = s[right], s[left]
             left += 1
             right -= 1
-        
-        return ''.join(s)
+        return ' '.join(s)
 
 # 238. Product of Array Except Self
 
